45445737930ac58ddf00db9ef4aa35da47b4bcea
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2017-09-01 15:10:28 +0530

    Update and rename nv2.c to pmm.c

---
+++ pmm.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{ 
+ int num;
+ printf("enter an integer");
+ scanf("%d",&num);
+ if(num%2==0)
+ {
+ printf("%d is even",num);
+ }https://github.com/vijayaprabha707/viji/create/master/nv2.c
+ else
+ {
+ printf("%d is odd",num);
+ }
+ return 0;
+ }
