b5cec6b2a2eb4200b5ab874e0345302f7c0349ce
vijayaprabha707 <30829618+vijayaprabha707@users.noreply.github.com>
2017-09-04 17:21:17 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int num;
+ printf("enter an integer");
+ scanf("%d",&num);
+ if(num%2==0)
+ {
+ printf("%d is even",num);
+ }
+ else
+ {
+ printf("%d is odd");
+ }
+ return 0;
+ }
+ 
